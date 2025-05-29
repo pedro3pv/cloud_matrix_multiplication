87abb15c2a95b5592da4bb6a53ce04f1510d4832
Pedro Augusto <pedro_atm3p@hotmail.com>
2025-05-28 21:30:44 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api_services import process_matrix_multiplication
+
+
+@api_view(['POST'])
+def matrix_multiplication(request):
+    result, status_code = process_matrix_multiplication(request.data)
+    return Response(result, status=status_code)
+
 
 def home(request):
     return render(request, 'home.html')
