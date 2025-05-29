ce23aa3fa0da4e18bd4f1351d0a86dd77ce20bb2
Pedro Augusto <pedro_atm3p@hotmail.com>
2025-05-28 21:12:17 -0300

    add route for home page

--- cloud_matrix_multiplication/urls.py
+++ cloud_matrix_multiplication/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pagina/', views.home, name='home'),
 ]
