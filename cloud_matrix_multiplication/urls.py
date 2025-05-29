8957edb48479a84bab9441d35422362c5af9e081
Pedro Augusto <pedro_atm3p@hotmail.com>
2025-05-28 21:30:08 -0300

    refactoring for new route

--- cloud_matrix_multiplication/urls.py
+++ cloud_matrix_multiplication/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 import views
+from api_views import MatrixMultiplicationView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('api/multiply-matrices/', MatrixMultiplicationView.as_view(), name='matrix_multiplication'),
 ]
