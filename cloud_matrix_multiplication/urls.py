336bf5a28c867674ef9da36f2005afedb23d3fce
Pedro Augusto <pedro_atm3p@hotmail.com>
2025-05-28 21:14:56 -0300

    disable admin route and change home route

--- cloud_matrix_multiplication/urls.py
+++ cloud_matrix_multiplication/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pagina/', views.home, name='home'),
+    # path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
